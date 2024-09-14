0b3ae36a9dc59558959dbd2a3dd09eb020babbbc
das-agung <das.agung13@gmail.com>
2024-09-14 22:28:09 +0700

    use state for themeToggle

--- src/components/ThemeToggle/ThemeToggle.tsx
+++ src/components/ThemeToggle/ThemeToggle.tsx
@@ -1,46 +1,46 @@
 import { ActionIcon, Center, Menu, rem, useMantineColorScheme, useMantineTheme } from '@mantine/core';
 import { IconChevronDown, IconMoon, IconSun } from '@tabler/icons-react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const ThemeToggle: React.FC = () => {
   const theme = useMantineTheme();
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
 
-  const SunIcon = () => {
-    return (
-      <IconSun
-        style={{ width: rem(20), height: rem(20) }}
-        stroke={2.5}
-        color={colorScheme === 'dark' ? theme.colors.dark[1] : theme.colors.yellow[4]}
-      />
-    )
-  };
+  const SunIcon = 
+    <IconSun
+      style={{ width: rem(20), height: rem(20) }}
+      stroke={2.5}
+      color={colorScheme === 'dark' ? theme.colors.dark[1] : theme.colors.yellow[4]}
+    />
 
-  const MoonIcon = () => {
-    return (
-      <IconMoon
-        style={{ width: rem(20), height: rem(20) }}
-        stroke={2.5}
-        color={colorScheme === 'dark' ? theme.colors.dark[1] : theme.colors.yellow[4]}
-      />
-    )
-  };
+  const MoonIcon = 
+    <IconMoon
+      style={{ width: rem(20), height: rem(20) }}
+      stroke={2.5}
+      color={colorScheme === 'dark' ? theme.colors.dark[1] : theme.colors.yellow[4]}
+    />
+
+  const [ activeIcon, setActiveIcon ] = useState<JSX.Element>();
+  const [ inactiveIcon, setInactiveIcon ] = useState<JSX.Element>();
+
+  useEffect(() => {
+    setActiveIcon(colorScheme === 'dark' ? MoonIcon : SunIcon);
+    setInactiveIcon(colorScheme !== 'dark' ? MoonIcon : SunIcon);
+  }, [colorScheme])
 
   return (
     <Menu key="user-toggle" trigger="hover" transitionProps={{ exitDuration: 0 }} withinPortal>
       <Menu.Target>
         <Center>
           <ActionIcon variant='subtle' radius='xl' style={{ cursor: 'pointer'}}>
-            {colorScheme === 'dark' && <MoonIcon />}
-            {colorScheme === 'light' && <SunIcon />}
+            {activeIcon}
           </ActionIcon>
           <IconChevronDown size="0.9rem" stroke={1.5} />
         </Center>
       </Menu.Target>
       <Menu.Dropdown>
         <Menu.Item onClick={() => toggleColorScheme()} style={{ cursor: 'pointer'}}>
-          {colorScheme !== 'dark' && <MoonIcon />}
-          {colorScheme !== 'light' && <SunIcon />}
+          {inactiveIcon}
         </Menu.Item>
       </Menu.Dropdown>
     </Menu>
